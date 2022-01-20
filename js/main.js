68594f185c0ef7ea72b88c50b956c5ef56589a20
1MalikErdas <302891760@student.rocmondriaan.nl>
2022-01-21 00:34:28 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log('file.loaded')
+
+const result = prompt('Wat is je naam');
+alert(`Jouw naam is ${result}`);
+
+const result1 = confirm('Deze website gebruikt cookies, wilt u doorgaan?');
+if (result) { alert('Je bent het er mee eens');}
+else { alert('Je bent het niet eens'); }
+
+
+
